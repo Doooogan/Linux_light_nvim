4ae97d9c78665ade3688d69f729e221e2c630684
Doogan <dyl.plu@gmail.com>
2025-07-26 21:34:19 -0400

    more fixes

--- init.lua
+++ init.lua
@@ -5,11 +5,8 @@ require("plugins")
 -- Load core user config (colors, keymaps, LSP, etc.)
 require("config.colorscheme")
 require("config.keybinds")
-<<<<<<< HEAD
 require("config.filetype")
-=======
 require("config.lsp")
->>>>>>> 4e334bb9fd83fe389f375cd2d10e112367f55170
 -- Load plugin-specific configs (if not already loaded via `use { config = ... }`)
 -- Only needed for things not lazy-loaded
 -- require("config.telekasten_config")
